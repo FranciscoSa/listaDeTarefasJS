3e1039aa32cae85cc30014c2ac1219ec624ef7c6
Francisco Santana Cardoso <fsantanagy@gmail.com>
2023-12-16 19:08:25 -0300

    Criada a estrutura inical

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from "react"
 
 function App() {
+  const [tarefa, setTarefa] = useState('')
+  const [listaDeTarefas, setListaDeTarefas] = useState([])
+
+  function salvarTarefa(event){
+    event.preventDefault()
+    alert("Salvo com sucesso")
+  }
+
   return (
     <div>
-      <h1>Start</h1>
+      <h1>Adicionar Tarefa</h1>
+      
+      <form onSubmit={salvarTarefa}>
+        <label>Tarefa:</label><br/>
+        <input placeholder = "Digite a tarefa"></input><br/><br/>
+        <button type="submit">Salvar Tarefa</button>
+      </form>
+
+      <div>
+        <h1>Lista de Tarefas:</h1>
+        <ul>
+          {listaDeTarefas.map(tarefa => (
+            <li>{tarefa}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+
 }
 
 export default App;
